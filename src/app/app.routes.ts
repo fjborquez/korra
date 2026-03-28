69ff636dcc3a511bc9632bfa669dbb2734ff5d36
Francisco Bórquez <fborquez@outlook.com>
2026-03-28 17:49:11 -0300

    auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import {Routes} from '@angular/router';
 import {Login} from './sections/login/login';
 import {Houses} from './houses';
 import {ProductDetail} from './product-detail';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: Login},
-  {path: 'inventory', component: Inventory},
-  {path: 'houses', component: Houses},
-  {path: 'product/:id', component: ProductDetail}
+  {path: 'inventory', component: Inventory, canActivate: [AuthGuard]},
+  {path: 'houses', component: Houses, canActivate: [AuthGuard]},
+  {path: 'product/:id', component: ProductDetail, canActivate: [AuthGuard]}
 ];
