3e3d2a2b6e253dfaa3497cd25daa427fc6dd538b
Francisco Bórquez <fborquez@outlook.com>
2026-04-10 19:56:26 -0400

    configurations

--- src/app/partials/configuration/configuration.ts
+++ src/app/partials/configuration/configuration.ts
@@ -1,6 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { ConfigurationService } from '../../services/configuration.service';
+import { LoginService } from '../../services/login.service';
+import { dateToChileanFormat } from '../../functions/date-to-chilean-format.function';
+import { Response } from '../../interfaces/response.interface';
 
 @Component({
   selector: 'app-configuration',
@@ -12,16 +16,31 @@ import { MatIconModule } from '@angular/material/icon';
   `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Configuration {
+export class Configuration implements OnInit {
   private fb = inject(FormBuilder);
+  private configurationService = inject(ConfigurationService);
+  private loginService = inject(LoginService);
+
   cancelled = output<void>();
   saved = output<void>();
   configForm = this.fb.group({
     name: ['', [Validators.required]],
     lastname: ['', [Validators.required]],
     date_of_birth: [new Date(), [Validators.required]],
-    password: ['', [Validators.required]]
+    password: ['']
   });
+
+  ngOnInit(): void {
+    this.configurationService.get(this.loginService.getUserId())
+      .subscribe((response: Response) => {
+        const configurationValues = response.message;
+        this.configForm.patchValue({
+          name: configurationValues.name,
+          lastname: configurationValues.lastname,
+          date_of_birth: configurationValues.date_of_birth
+        });
+      });
+  }
 
   handleCancel() {
     this.cancelled.emit();
@@ -29,7 +48,24 @@ export class Configuration {
 
   onSubmit() {
     if (this.configForm.valid) {
-      console.log(this.configForm.value);
+      const formValues = this.configForm.value;
+      const values = {
+        name: formValues.name,
+        lastname: formValues.lastname,
+        password: formValues.password,
+      } as {
+        name: string;
+        lastname: string;
+        password: string;
+        date_of_birth?: string;
+      };
+
+      if (formValues.date_of_birth) {
+        values.date_of_birth = dateToChileanFormat(String(formValues.date_of_birth));
+      }
+
+      this.configurationService.edit(this.loginService.getUserId(), values)
+        .subscribe(() => this.saved.emit());
     }
   }
 }
